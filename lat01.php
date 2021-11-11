b96e4f3473cedfcdc5ef86897a1bd5a59880cfa5
FarrelRizkyRamadhani <87293573+FarrelRizkyRamadhani@users.noreply.github.com>
2021-11-11 13:08:52 +0700

    Add files via upload

---
+++ lat01.php
@@ -0,0 +1,13 @@
+<?php
+    echo 'belajar PHP <br>';
+
+    echo 'saya siswa smk';
+
+    echo '<h1 style="background-color:yellowgreen">Belajar PHP itu mudah</h1>
+          <p style="background-color:wheat">Saya belajar program</p>
+    ';
+
+    echo 'Saya belajar'.' PHP'.'<br>';
+
+    echo 'Tahun 2019';
+?>
